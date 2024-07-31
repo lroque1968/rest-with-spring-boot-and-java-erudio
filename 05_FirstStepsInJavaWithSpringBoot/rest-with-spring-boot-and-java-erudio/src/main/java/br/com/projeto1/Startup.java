7107bce19e1c00e75dadbaadb8ff6d163edc699c
Luis Roque <luisroque@tic.ufrj.br>
2024-07-31 10:13:55 -0300

    Inserção de mensagem na classe Startup

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/projeto1/Startup.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/projeto1/Startup.java
@@ -1,5 +1,17 @@
 package br.com.projeto1;
 
+/*
+  	Primeira parte do projeto do curso 2 de Java com Rest.
+  	Montagem do projeto com Spring Starter Project.
+  	
+  	Teste da conexão com o localhost:8080/greeting
+  						 e localhost:8080/greeting?name=luis
+  						 
+  	Projeto salvo no Git
+  	
+  	Continua no projeto Calculadora.
+*/
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
